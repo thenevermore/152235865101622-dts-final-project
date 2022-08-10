6de4ddee70d91189a21e5e8eca2438583ba6b748
thenevermore <nurhadiarian@gmail.com>
2022-08-10 17:27:04 +0700

    pakpos/fix base URL

--- src/api/gsmarena.js
+++ src/api/gsmarena.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const baseURL = 'https://api-mobilespecs.azharimm.site/v2/latest';
-const baseURL = 'http://localhost:3000/data.json';
+const baseURL = 'https://api-mobilespecs.azharimm.site/v2/latest';
+//const baseURL = 'http://localhost:3000/data.json';
 const baseDetail = 'http://api-mobilespecs.azharimm.site/v2/';
 
 const gsmdb = axios.create({
